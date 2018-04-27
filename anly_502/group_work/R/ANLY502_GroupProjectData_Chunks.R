396eb51f39ca0123c4e4f92f7761b99efc9d8d92
outcastgeek <outcastgeek+git@gmail.com>
2018-04-27 15:43:05 -0500

    Working on Group Work Data Description (in-progress)

---
+++ anly_502/group_work/R/ANLY502_GroupProjectData_Chunks.R
@@ -0,0 +1,19 @@
+
+## @knitr installLibraries
+
+#install.packages("knitr")
+#install.packages("readxl")
+#install.packages("ggplot2")
+#install.packages("pastecs")
+#install.packages("lattice")
+#install.packages("lmtest")
+
+## @knitr loadLibraries
+
+library(readxl)
+
+library(ggplot2)
+
+library(stats)
+
+library(pastecs)
